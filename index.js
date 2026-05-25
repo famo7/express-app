81d2747623a4ae8e0b4bd33552d156407be142e5
famo7 <famo1901@gmail.com>
2026-05-25 21:02:49 +0200

    feat: update greeting message in the root route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World updated!</h1>')
+  res.send('<h1>Hello World updated 2!</h1>')
 })
 
 const PORT = 8080
